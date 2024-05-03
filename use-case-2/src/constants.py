551d4a8fc0d8db8670af4a8880cbce91f33bb90c
schiesem <marvin.schieseck@gmx.de>
2024-05-03 17:54:46 +0200

    repo cleaned up

--- use-case-2/src/constants.py
+++ use-case-2/src/constants.py
@@ -4,4 +4,5 @@ RAW_DATA_DIR = "D:\\work-hsu\\08_code_programms\\ai-in-production-example-develo
 PROCESSED_DATA_DIR = "D:\\work-hsu\\08_code_programms\\ai-in-production-example-development\\use-case-2\\data\\processed"
 
 
+
 print(f"Import form {__name__} is working")
