6d00435c03269305b0cc5dd1f555905e3b3b36dc
schiesem <marvin.schieseck@gmx.de>
2024-05-06 21:14:08 +0200

    notebook extended

--- use-case-2/main.py
+++ use-case-2/main.py
@@ -1,16 +1,3 @@
 if __name__ == "__main__":
      #------------------------- imports -------------------------#
-    from os.path import join
-    from copy import copy 
-    from tqdm import tqdm
-
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import pandas as pd
-    from scipy import signal
-
-
-    import src.data.utils as dt
-    import src.visualization.utils as vz
-    
     print("Main End")
